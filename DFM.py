a9f709bfd03d535ae712970741f4af4e0339ae49
Darkness Comet <siriusblackmaz@gmail.com>
2021-01-12 23:23:09 -0400

    first commit

---
+++ DFM.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy as np
+
+class DemographicFileMaker:
+
+    def __init__(self):
+        pass
+    
+    def readAllFiles(self):
+        pass
+
+    def writeOutput(self):
+        pass
+
+    def makeReport(self):
+        pass
+
+if __name__ == "__main__":
+
+    # Create a object.
+    dfm = DemographicFileMaker()
+
+    # Read all needed files.
+    dfm.readAllFiles()
+
+    # Do main processes
+    dfm.makeReport()
+
+    # Write output file (Mockup STAR.xlsx)
+    dfm.writeOutput()
